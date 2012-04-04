92db1ee28ab5d816421e518df7c2e830637142b3
Noom <noom@figabyte.com>
2012-04-04 23:27:30 +0700

    Added production config files

---
+++ application/config/production/mockuphappen.php
@@ -0,0 +1,36 @@
+<?php //These will be used when $config['mockuphappen_enable']==TRUE
+$config['mockuphappen_request_array'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_install_app'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_install_page'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_user_id'] = array('status' => 'OK', 'success' => TRUE, 'user_id' => TRUE);
+$config['mockuphappen_request_user_facebook_id'] = array('status' => 'OK', 'success' => TRUE, 'user_facebook_id' => TRUE);
+$config['mockuphappen_request_facebook_page_id'] = array('status' => 'OK', 'success' => TRUE, 'facebook_page_id' => TRUE);
+$config['mockuphappen_request_page_id'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_log_user'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_authenticate'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_user_session'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_user'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_page_users'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_app_users'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_add_limit_service'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_count_limit_service'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_bar'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_get_started'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_setting_template'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_show_notification'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_read_notification'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_add_achievement_infos'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_login'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_facebook_tab_url'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_create_invite'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_accept_invite'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_invite_list'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_current_campaign'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_user_classes'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_add_campaign'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_page_role'] = array('status' => 'OK', 'success' => TRUE);
+$config['mockuphappen_request_campaign_list'] = array('status' => 'OK', 'success' => TRUE);
+
+$config['mockuphappen_facebook_page_id'] = '';
+$config['mockuphappen_app_install_id'] = '1';
+$config['mockuphappen_app_install_secret_key'] = '11111111111111111111111111111111';
